2127ad6e3f86810bf3331f7b8493af0ed593df25
ducdapda <duc.phan622@gmail.com>
2022-01-24 18:08:34 +0700

    update

--- aaa.js
+++ aaa.js
@@ -1,15 +1,42 @@
-const moment = require("moment-timezone");
-console.log("🚀 ~ file: aaa.js ~ line 2 ~ moment now", moment().toDate());
+"use strict";
+const nodemailer = require("nodemailer");
 
-const startDate = moment().startOf("month").subtract(12, "months").toDate();
-console.log("🚀 ~ file: aaa.js ~ line 4 ~ startDate", startDate);
-const endDate = moment().endOf("month").toDate();
-console.log("🚀 ~ file: aaa.js ~ line 6 ~ endDate", endDate);
+// async..await is not allowed in global scope, must use a wrapper
+async function main() {
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  let testAccount = await nodemailer.createTestAccount();
 
-const prevStartDate = moment(startDate).subtract(1, "years").toDate();
-console.log("🚀 ~ file: aaa.js ~ line 3 ~ prevStartDate", prevStartDate);
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "coincardbomay@gmail.com", //Tài khoản gmail vừa tạo
+      pass: "ducdapdachoicanbuco", //Mật khẩu tài khoản gmail vừa tạo
+    },
+    tls: {
+      // do not fail on invalid certs
+      rejectUnauthorized: false,
+    },
+  });
 
-const prevEndDate = moment("2012-02-29T16:59:59.999Z")
-  .subtract(1, "years")
-  .toDate();
-console.log("🚀 ~ file: aaa.js ~ line 5 ~ prevEndDate", prevEndDate);
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: "coincardbomay@gmail.com", // sender address
+    to: "duc.phan622@gmail.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+
+main().catch(console.error);
