46255d128b699ab119bfd272c4729ed3cf093d7d
ducdapda <duc.phan622@gmail.com>
2022-01-25 09:12:48 +0700

    update

--- aaa.js
+++ aaa.js
@@ -1,13 +1,8 @@
-"use strict";
 const nodemailer = require("nodemailer");
 
-// async..await is not allowed in global scope, must use a wrapper
+// async..await is not allowed in global scope, must use a wrapper 🅰️
 async function main() {
-  // Generate test SMTP service account from ethereal.email
-  // Only needed if you don't have a real mail account for testing
   let testAccount = await nodemailer.createTestAccount();
-
-  // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
@@ -17,12 +12,11 @@ async function main() {
       pass: "ducdapdachoicanbuco", //Mật khẩu tài khoản gmail vừa tạo
     },
     tls: {
-      // do not fail on invalid certs
+      // do not fail on invalid certs 🚩
       rejectUnauthorized: false,
     },
   });
 
-  // send mail with defined transport object
   let info = await transporter.sendMail({
     from: "coincardbomay@gmail.com", // sender address
     to: "duc.phan622@gmail.com", // list of receivers
@@ -32,11 +26,7 @@ async function main() {
   });
 
   console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  // Preview only available when sending through an Ethereal account
   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 }
 
 main().catch(console.error);
