9ee1affe8fad7989512668f290313402f107b285
phantaiduc <duc.phan622@gmail.com>
2022-03-06 11:36:01 +0700

    update

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
-//example catch error
-const error = new Error("error");
-try {
-  throw error;
-} catch (err) {
-  console.log(err);
-}
-console.log(error);
-export default "hrllo";
-export const name = "hello";
+console.log("Hello World");
