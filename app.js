9ce9c32068e18ed4ef98fc44d37ce34d358501ce
duc2k1 <duc.phan622@gmail.com>
2022-03-30 10:16:12 +0700

    update

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-const rxjs = require("rxjs");
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  getName() {
+    return this.name;
+  }
+  getAge() {
+    return this.age;
+  }
+}
+
+const person = new Person("Phan Duc", 21);
+console.log(person.getName());
